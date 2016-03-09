b885265b5720e2febc84a0344644c5699dc95ff4
James Kelleher <jk3047@nyu.edu>
2016-03-09 18:03:06 -0500

    Initial Commit

---
+++ project-telephone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project-telephone
+//
+//  Created by James Kelleher on 3/9/16.
+//  Copyright © 2016 Kelleher & Fam, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
