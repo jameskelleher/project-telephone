83e14005e0bac15b8aff4fefb60ad0bf8914867b
James Kelleher <jk3047@nyu.edu>
2016-03-09 18:30:59 -0500

    added some butts

--- project-telephone/LoginViewController.swift
+++ project-telephone/LoginViewController.swift
@@ -15,9 +15,12 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
         
         configureFacebook()
+        
+//        while true {
+//            print("butts")
+//        }
     }
     
     override func didReceiveMemoryWarning() {
